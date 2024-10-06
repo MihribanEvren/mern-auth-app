68cf995b8253ab4e1c9795660082971fc649105c
Mihriban Evren <mihribanevrenn@gmail.com>
2024-10-06 09:58:00 +0300

    initialize project

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
