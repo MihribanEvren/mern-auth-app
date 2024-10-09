8914cc1c3c07c8bb09598848cc68207f43d3e285
Mihriban Evren <mihribanevrenn@gmail.com>
2024-10-09 16:45:02 +0300

    prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import morgan from 'morgan';
 import { connectDB } from './config/db.js';
 import router from './router/route.js';
@@ -10,12 +11,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.disable('x-powered-by');
 
 app.use('/api', router);
+
+if (process.env.NODE_ENV === 'production') {
+}
 
 app.listen(PORT, () => {
   connectDB();
